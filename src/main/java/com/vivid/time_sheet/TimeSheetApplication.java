cd23c0b708ad8bd3c18ee7cba056fa0a519a74ab
VISHNUVARDHAN25012002 <116712471+VISHNUVARDHAN25012002@users.noreply.github.com>
2024-10-25 11:11:13 +0530

    First Push

---
+++ src/main/java/com/vivid/time_sheet/TimeSheetApplication.java
@@ -0,0 +1,13 @@
+package com.vivid.time_sheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeSheetApplication.class, args);
+    }
+
+}
